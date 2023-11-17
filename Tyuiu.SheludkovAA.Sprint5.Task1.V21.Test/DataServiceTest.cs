a154ea8c5eb3272e7ebacaa2940dcec8cb2fb5c1
Tyuiu.SheludkovAA@outlook.com <Tyuiu.SheludkovAA@outlook.com>
2023-11-17 21:51:35 +0500

    Task1 V21

---
+++ Tyuiu.SheludkovAA.Sprint5.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SheludkovAA.Sprint5.Task1.V21.Lib;
+using System.IO;
+
+namespace Tyuiu.SheludkovAA.Sprint5.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\alex3\source\repos\Tyuiu.SheludkovAA.Sprint5\Tyuiu.SheludkovAA.Sprint5.Task1.V21\bin\Debug\OutPutFileTask1.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+    }
+}
---
+++ Tyuiu.SheludkovAA.Sprint5.Task1.V21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SheludkovAA.Sprint5.Task1.V21.Lib;
+
+namespace Tyuiu.SheludkovAA.Sprint5.Task1.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #5 | Выполнил: Шелудков А. А. | АСОиУб-23-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Шелудков А. А. | АСОиУб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int start = -5;
+            int end = 5;
+            Console.WriteLine("Начальная координата : " + start);
+            Console.WriteLine("Конечная координата : " + end);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(start, end);
+            Console.WriteLine("Файл :" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
